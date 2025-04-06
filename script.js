77730e43766c6d83fe3ee8ac3d4f279b376e0823
zcafe <zcafe2024@gmail.com>
2025-04-06 19:52:54 +0800

    更新錯誤6"

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const pointUrl = 'https://script.google.com/macros/s/AKfycbzl7YLoeE0h2Oib2Ud2-UhTNomoggXk_OBKHX-kXP-ORfAlxyGH_VZClDR9eqoQTfWc/exec';
+const pointUrl = 'https://AKfycbzx3t6ktbHZMUBGzGsKyfPnI3jyngSTA60NuoMnOlczJf4Eq_SckJp0eAbDaQEhIRNZdA/exec';
 const recordUrl = 'https://script.google.com/macros/s/AKfycbzIktMLmiiJlANmXjPE-up8EGNiUD1PwJbAFT-Ffr3iu0i-NAQ77SOIzTF7I0VTHYcCYA/exec'; 
 
 function checkAll() {
